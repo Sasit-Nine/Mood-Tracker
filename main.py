5f3e41c84bf8a09babfe75a25ab2a6a10eb242b9
Monai15 <monai152548@gmail.com>
2025-03-09 03:17:13 +0700

    add: แสดง emoji เพิ่ม

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from kivy.properties import ObjectProperty
 from components.music_service import suggest_music
 from components.DeezerPlayer import DeezerPlayer
 
+
 # from kivy.core.window import Window
 # from kivy.config import Config
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 
-Window.fullscreen = True
+Window.fullscreen = False
 deezer_player = DeezerPlayer().play_preview
 
 
